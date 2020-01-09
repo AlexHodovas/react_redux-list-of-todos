dda40ce9bc4b87a6b22f2580f0bae74ce832381c
AlexHodovas <o.hodovas@gmail.com>
2020-01-09 17:51:53 +0200

    Updates

---
+++ precache-manifest.e239e4b6f70d22aef54319e768c53e79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8425d9d47f60036079b48e731624a0a",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "b37c733ea2884260b737",
+    "url": "/react_redux-list-of-todos/static/css/main.5fc3ef66.chunk.css"
+  },
+  {
+    "revision": "aa3cdf8cc241a9489219",
+    "url": "/react_redux-list-of-todos/static/js/2.cda8485f.chunk.js"
+  },
+  {
+    "revision": "b37c733ea2884260b737",
+    "url": "/react_redux-list-of-todos/static/js/main.497e7c2d.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
